e7027cc94f0e273f2fbad24f0ba409250a0a116c
wshuai294 <wshuai294@gmail.com>
2025-02-24 16:22:04 -0800

    profile smk

---
+++ collect_motifs.py
@@ -0,0 +1,34 @@
+"""
+Given the folder motifs/, enumerage all the motifs and collect them into a single file.
+"""
+
+import os
+import sys
+import pandas as pd
+
+def collect_motifs(folder, all_motif, MIN_FRAC, MIN_detect):
+    motifs = []
+    for file in os.listdir(folder):
+        if file.endswith(".csv"):
+            motif = pd.read_csv(os.path.join(folder, file), sep = ",")
+            ## add a new column for the motif indentifier, which is the combination of motif and pos
+            motif = motif[(motif['fraction'] > MIN_FRAC) & (motif['nDetected'] > MIN_detect)]
+            # print ("no of motifs", len(motif))
+            motif['indentifier'] = motif['motifString'] + "_" + motif['centerPos'].astype(str)
+            motifs.append(motif)
+    motifs = pd.concat(motifs)
+    ## only keep one row for each motif indentifier
+    motifs = motifs.drop_duplicates(subset=['indentifier'])
+    motifs.to_csv(all_motif, index=False)
+    print ("no of motifs", len(motifs))
+
+
+if __name__ == "__main__":
+    MIN_FRAC = 0.6
+    MIN_detect = 1000
+    # folder = "/home/shuaiw/borg/all_test/motifs"
+    # all_motif = "/home/shuaiw/borg/all_test/test_motifs.csv"
+
+    folder = sys.argv[1]
+    all_motif = sys.argv[2]
+    collect_motifs(folder, all_motif, MIN_FRAC, MIN_detect)
--- motif_profile.py
+++ motif_profile.py
@@ -4,8 +4,8 @@ from Bio.Seq import Seq
 import xml.etree.ElementTree as ET
 import pandas as pd
 from scipy.stats import pearsonr
-import matplotlib.pyplot as plt
-
+# import matplotlib.pyplot as plt
+import sys
 
 
 def read_ref(ref):
@@ -29,12 +29,10 @@ def get_motif_sites(REF, motif_new, exact_pos, modified_loci):
     rev_modified_num = 0
 
     for r, contig in REF.items():
-        print (r)
         for site in nt_search(str(contig), motif_new)[1:]:
             # for i in range(site, site + motif_len):
             #     motif_sites[r + ":" + str(i) + "+"] = motif_new
             tag = r + ":" + str(site+exact_pos) + "+"
-            motif_sites[tag] = motif_new
             motif_loci_num += 1
             for_loci_num += 1
             if tag in modified_loci:
@@ -45,19 +43,41 @@ def get_motif_sites(REF, motif_new, exact_pos, modified_loci):
             1:
         ]:
             tag = r + ":" + str(site+rev_exact_pos) + "-"
-            motif_sites[tag] = motif_new
             motif_loci_num += 1
             rev_loci_num += 1
             if tag in modified_loci:
                 motif_modify_num += 1
                 rev_modified_num += 1
-    print ("for_loci_num", for_loci_num, for_modified_num, "forward modified ratio", for_modified_num/for_loci_num)
-    print ("rev_loci_num", rev_loci_num, rev_modified_num, "reverse modified ratio", rev_modified_num/rev_loci_num)
+    # print ("for_loci_num", for_loci_num, for_modified_num, "forward modified ratio", for_modified_num/for_loci_num)
+    # print ("rev_loci_num", rev_loci_num, rev_modified_num, "reverse modified ratio", rev_modified_num/rev_loci_num)
 
-    print ("motif_loci_num", motif_loci_num)
-    print ("motif_modify_num", motif_modify_num)
-    print ("modified_ratio", motif_modify_num/motif_loci_num)
-    return motif_sites
+    # print ("motif_loci_num", motif_loci_num)
+    # print ("motif_modify_num", motif_modify_num)
+    if motif_loci_num == 0:
+        ratio = 0
+    else:
+        ratio = motif_modify_num/motif_loci_num
+    if for_loci_num == 0:
+        for_ratio = 0
+    else:
+        for_ratio = for_modified_num/for_loci_num
+    if rev_loci_num == 0:
+        rev_ratio = 0
+    else:
+        rev_ratio = rev_modified_num/rev_loci_num
+    if len(modified_loci) == 0:
+        proportion_all_modified = 0
+    else:
+        proportion_all_modified = motif_modify_num/len(modified_loci)
+
+    if ratio > 0.4 and proportion_all_modified > 0.1:
+        print (motif_new, "modified_ratio", ratio, motif_modify_num, motif_loci_num, proportion_all_modified)
+
+    return [for_loci_num, for_modified_num,for_ratio,\
+            rev_loci_num, rev_modified_num, rev_ratio,\
+            motif_loci_num, motif_modify_num, ratio, proportion_all_modified]
+
+
 
 def get_modified_ratio(gff):
     ## read the gff file
@@ -79,16 +99,37 @@ def get_modified_ratio(gff):
     return modified_loci
 
          
+if __name__ == "__main__":
+    # motif_new = "CTGCAG"
+    # exact_pos = 5
+    score_cutoff = 30
+    # my_ref = "/home/shuaiw/methylation/data/published_data/fanggang/ref/C227.fa"
+    # gff = "/home/shuaiw/borg/bench/C227_native/gffs/CP011331.1.gff"
 
-motif_new = "CTGCAG"
-exact_pos = 5
-score_cutoff = 30
-my_ref = "/home/shuaiw/methylation/data/published_data/fanggang/ref/C227.fa"
-gff = "/home/shuaiw/borg/bench/C227_native/gffs/CP011331.1.gff"
+    # my_ref = "/home/shuaiw/borg/all_test//contigs/SR-VP_9_9_2021_81_5A_0_75m_PACBIO-HIFI_HIFIASM-META_19121_L.fa"
+    # gff = "/home/shuaiw/borg/all_test//gffs/SR-VP_9_9_2021_81_5A_0_75m_PACBIO-HIFI_HIFIASM-META_19121_L.gff"
+    # all_motifs = "/home/shuaiw/borg/all_test/test_motifs.csv"
+    # profile = "/home/shuaiw/borg/all_test/profiles/SR-VP_9_9_2021_81_5A_0_75m_PACBIO-HIFI_HIFIASM-META_19121_L.csv"
 
-REF = read_ref(my_ref)
-# print (REF)
-modified_loci = get_modified_ratio(gff)
-motif_sites = get_motif_sites(REF, motif_new, exact_pos, modified_loci)
+    my_ref = sys.argv[1]
+    gff = sys.argv[2]
+    all_motifs = sys.argv[3]
+    profile = sys.argv[4]
+
+    REF = read_ref(my_ref)
+    # print (REF)
+    modified_loci = get_modified_ratio(gff)
+    motifs = pd.read_csv(all_motifs)
+    data = []
+    for index, motif in motifs.iterrows():
+        motif_new = motif["motifString"]
+        exact_pos = motif["centerPos"]
+        motif_profile = get_motif_sites(REF, motif_new, exact_pos, modified_loci)
+        data.append([motif_new, exact_pos] + motif_profile)
+
+    df = pd.DataFrame(data, columns = ["motifString", "centerPos", "for_loci_num", "for_modified_num", "for_modified_ratio",\
+                                        "rev_loci_num", "rev_modified_num", "rev_modified_ratio",\
+                                        "motif_loci_num", "motif_modified_num", "motif_modified_ratio", "proportion"])
+    df.to_csv(profile, index=False)
 
 
