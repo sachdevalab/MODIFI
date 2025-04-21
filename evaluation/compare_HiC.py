7e47afc642d4ad19f1e7a3a673284aa996406fa3
wshuai294 <wshuai294@gmail.com>
2025-04-21 15:13:02 -0700

    cow map contig to bin

--- evaluation/compare_HiC.py
+++ evaluation/compare_HiC.py
@@ -1,8 +1,7 @@
-hic_file = "/home/shuaiw/borg/pengfan/hic_10mgs_linkages.csv"
-# host_sum = "/home/shuaiw/borg/pengfan/RuReacBro_20230708_11_72h_20_bin/host_summary.csv"
-host_sum = "/home/shuaiw/borg/pengfan/RuReacBro_20230708_Comb_RF_LR_bin/host_summary.csv"
-
+# -*- coding: utf-8 -*-
 import pandas as pd
+import os
+import sys
 
 def read_hic(hic_file):
     hic_linkages = {}
@@ -11,15 +10,23 @@ def read_hic(hic_file):
         hic_linkages[row['circular_element']] = row['bin']
     return hic_linkages
 
-def read_our(host_sum, score_cutoff = 0.45):
+def read_our(host_sum, ctg2bin_dict, score_cutoff = 0.45):
     df = pd.read_csv(host_sum)
     df = df[df['final_score'] > score_cutoff]
     our_linkages = {}
+    our_ctg_linkages = {}
     for index, row in df.iterrows():
-        our_linkages[row['plasmid']] = row['host']
-    return our_linkages
+        if row['host'] not in ctg2bin_dict:
+            ## raise error
+            print (f"contig {row['host']} is not in ctg2bin_dict")
+            sys.exit(1)
+        bin_name = ctg2bin_dict[row['host']]
+        our_linkages[row['plasmid']] = bin_name
+        our_ctg_linkages[row['plasmid']] = row['host']
+        # our_linkages[row['plasmid']] = row['host']
+    return our_linkages, our_ctg_linkages
 
-def compare_hic_our(hic_linkages, our_linkages):
+def compare_hic_our(hic_linkages, our_linkages, our_ctg_linkages, bin2ctg_dict):
     both_link = 0
     cosistency_num = 0
     for plasmid in our_linkages:
@@ -30,7 +37,7 @@ def compare_hic_our(hic_linkages, our_linkages):
         if our_linkages[plasmid] == hic_linkages[plasmid]:
             cosistency_num += 1
         else:
-            print (f"{plasmid} is not consistent: {our_linkages[plasmid]} vs {hic_linkages[plasmid]}")
+            print (f"{plasmid} is not consistent: {our_linkages[plasmid]} ({our_ctg_linkages[plasmid]}) vs Hi-C {hic_linkages[plasmid]} : ({bin2ctg_dict[hic_linkages[plasmid]]})")
     if both_link == 0:
         cosistency_rate = 0
     else:
@@ -41,65 +48,84 @@ def compare_hic_our(hic_linkages, our_linkages):
     print ("our link num", len(our_linkages))
     return both_link, cosistency_num, cosistency_rate, len(our_linkages)
 
-data = []
-for cutoff in range(0, 16):
-    my_cutoff = cutoff / 20
-    hic_linkages = read_hic(hic_file)
-    our_linkages = read_our(host_sum, my_cutoff)
-    print (f"cutoff: {my_cutoff}")
-    both_link, cosistency_num, consis_rate, our_num = compare_hic_our(hic_linkages, our_linkages)
-    data.append([my_cutoff, both_link, cosistency_num, consis_rate, our_num])
-df = pd.DataFrame(data, columns=['cutoff', 'both_link', 'cosistency_num', 'consis_rate', 'our_num'])
+def main():
+    ctg2bin_dict, bin2ctg_dict = load_ctg2bin(ctg2bin)
+    data = []
+    for cutoff in range(0, 16):
+        my_cutoff = cutoff / 20
+        hic_linkages = read_hic(hic_file)
+        our_linkages, our_ctg_linkages = read_our(host_sum, ctg2bin_dict, my_cutoff)
+        print (f"cutoff: {my_cutoff}")
+        both_link, cosistency_num, consis_rate, our_num = compare_hic_our(hic_linkages, our_linkages, our_ctg_linkages, bin2ctg_dict)
+        data.append([my_cutoff, both_link, cosistency_num, consis_rate, our_num])
+    df = pd.DataFrame(data, columns=['cutoff', 'both_link', 'cosistency_num', 'consis_rate', 'our_num'])
 
-### plot the consistency rate line plot
-## plot another subplot with the our link num
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style="whitegrid")
+    ### plot the consistency rate line plot
+    ## plot another subplot with the our link num
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.set(style="whitegrid")
 
 
-plt.figure(figsize=(5, 9))  # Adjust the figure size to accommodate the third subplot
+    plt.figure(figsize=(5, 9))  # Adjust the figure size to accommodate the third subplot
 
-## plot three subplots
-plt.subplot(3, 1, 1)
-plt.plot(df['cutoff'], df['consis_rate'], marker='o')
-plt.title('Ratio of Consistency Linkages')
-plt.xlabel('Cutoff')
-plt.ylabel('Ratio of Consistency Linkages')
+    ## plot three subplots
+    plt.subplot(3, 1, 1)
+    plt.plot(df['cutoff'], df['consis_rate'], marker='o')
+    plt.title('Ratio of Consistency Linkages')
+    plt.xlabel('Cutoff')
+    plt.ylabel('Ratio of Consistency Linkages')
 
-plt.subplot(3, 1, 2)
-plt.plot(df['cutoff'], df['cosistency_num'], marker='o')
-plt.title('Number of Consistency Linkages')
-plt.xlabel('Cutoff')
-plt.ylabel('Number of Consistency Linkages')
+    plt.subplot(3, 1, 2)
+    plt.plot(df['cutoff'], df['cosistency_num'], marker='o')
+    plt.title('Number of Consistency Linkages')
+    plt.xlabel('Cutoff')
+    plt.ylabel('Number of Consistency Linkages')
 
-plt.subplot(3, 1, 3)
-plt.plot(df['cutoff'], df['our_num'], marker='o', color='green')  # Plot our_num
-plt.title('Number of Our Linkages')
-plt.xlabel('Cutoff')
-plt.ylabel('Number of Our Linkages')
+    plt.subplot(3, 1, 3)
+    plt.plot(df['cutoff'], df['our_num'], marker='o', color='green')  # Plot our_num
+    plt.title('Number of Our Linkages')
+    plt.xlabel('Cutoff')
+    plt.ylabel('Number of Our Linkages')
 
-plt.tight_layout()
-plt.savefig('../tmp/both_linkages.png')
+    plt.tight_layout()
+    plt.savefig('../tmp/both_linkages.png')
 
-# plt.figure(figsize=(5, 6))
-# ## plot two subplots
-# plt.subplot(2, 1, 1)
-# plt.plot(df['cutoff'], df['consis_rate'], marker='o')
-# plt.title('Ratio of Consistency Linkages')
-# plt.xlabel('Cutoff')
-# plt.ylabel('Ratio of Consistency Linkages')
-# plt.subplot(2, 1, 2)
-# plt.plot(df['cutoff'], df['cosistency_num'], marker='o')
-# plt.title('Number of Consistency Linkages')
-# plt.xlabel('Cutoff')
-# plt.ylabel('Number of Consistency Linkages')
+def contig2bin(bin_dir, ctg2bin):
+    out = open(ctg2bin, "w")
+    out.write("contig,bin\n")
+    for bin_file in os.listdir(bin_dir):
+        if bin_file.endswith(".fa"):
+            bin_name = bin_file[:-3]
+            with open(os.path.join(bin_dir, bin_file), "r") as f:
+                for line in f:
+                    if line.startswith(">"):
+                        contig_name = line.strip().split(" ")[0][1:]
+                        print(f"{contig_name},{bin_name}", file=out)
+    out.close()
+    print(f"contig2bin file is saved to {ctg2bin}")
 
-# plt.tight_layout()
-# plt.savefig('../tmp/both_linkages.png')
+def load_ctg2bin(ctg2bin):
+    ctg2bin_dict = {}
+    with open(ctg2bin, "r") as f:
+        for line in f:
+            if line.startswith("contig"):
+                continue
+            contig, bin_name = line.strip().split(",")
+            ctg2bin_dict[contig] = bin_name
+    bin2ctg_dict = {}
+    for contig, bin_name in ctg2bin_dict.items():
+        if bin_name not in bin2ctg_dict:
+            bin2ctg_dict[bin_name] = []
+        bin2ctg_dict[bin_name].append(contig)
+    return ctg2bin_dict, bin2ctg_dict
 
-# plt.plot(df['cutoff'], df['consis_rate'], marker='o')
-# plt.title('Consistency Rate of HiC and Our Linkages')
-# plt.xlabel('Cutoff')
-# plt.ylabel('Consistency Rate')
-# plt.savefig('../tmp/consistency_rate.png')
+if __name__ == "__main__":
+    hic_file = "/home/shuaiw/borg/pengfan/hic_10mgs_linkages.csv"
+    host_sum = "/home/shuaiw/borg/pengfan/RuReacBro_20230708_11_72h_20_bin/host_summary.csv"
+    bin_dir = "/groups/diamond/projects/animal/rumen/RuReacBro20203/analysis/RuReacBro2023_CircCont/bins/Final_Genomes_qc_rmcirc/sequence"
+    ctg2bin = "/home/shuaiw/borg/pengfan/ctg2bin.csv"
+    # host_sum = "/home/shuaiw/borg/pengfan/RuReacBro_20230708_Comb_RF_LR_bin/host_summary.csv"
+
+    main()
+    # contig2bin(bin_dir, ctg2bin)
