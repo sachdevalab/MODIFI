2e7a6696fe05fc643c4a116cd7a78991e37ed0cd
wshuai294 <wshuai294@gmail.com>
2025-10-24 19:15:34 -0700

    folder clean

--- benchmark/novel_motif/find_novel_motif.py
+++ benchmark/novel_motif/find_novel_motif.py
@@ -200,4 +200,4 @@ all_rebase_motif = "/home/shuaiw/borg/paper/novel_motif/rebase.sort.motif.txt"
 sample_motif_file = "/home/shuaiw/borg/bench/soil/run1/all.motifs.csv"
 load_sample_motif(all_rebase_motif, sample_motif_file)
 
-## xxx
+## xxx dsfsadf
