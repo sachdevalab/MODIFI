70d0b66b60a04e1dc5709807a0eaeebd9dd48cef
wshuai294 <wshuai294@gmail.com>
2026-03-18 12:01:31 -0700

    run subreads

---
+++ benchmark/compare/batch_ipd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=ipd_summary_28   # Job name
+#SBATCH --partition=standard        # Partition name
+
+set -euo pipefail
+
+ref_dir=/home/shuaiw/borg/contigs/
+ref_set=(test_100.fa test_200.fa test_300.fa test_500.fa)
+outdir=/home/shuaiw/borg/paper/ipdsummary/soil_1/
+ipd_output=/home/shuaiw/borg/paper/ipdsummary/soil_1/ipd.out/
+threads="${SLURM_CPUS_ON_NODE:-64}"
+mkdir -p "$outdir"
+mkdir -p "$ipd_output"
+
+for ref in "${ref_set[@]}"; do
+    prefix="$outdir/$(basename "$ref" .fa)"
+    ipd_prefix="$ipd_output/$(basename "$ref" .fa)"
+    align_bam="$prefix.align.subreads.bam"
+    ref_path="$ref_dir/$ref"
+
+    echo "Running ipdSummary for $ref_path"
+    echo "    /usr/bin/time -v -o $ipd_prefix.ipdSummary.time ~/smrtlink/ipdSummary $align_bam --reference $ref_path --debug --numWorkers 1 --gff $ipd_prefix.gff --csv $ipd_prefix.csv --methylFraction --outfile $ipd_prefix.out"
+
+    /usr/bin/time -v -o "$ipd_prefix.ipdSummary.time" \
+        ~/smrtlink/ipdSummary "$align_bam" \
+        --reference "$ref_path" \
+        --numWorkers $threads \
+        --gff "$ipd_prefix.gff" \
+        --csv "$ipd_prefix.csv" \
+        --methylFraction \
+        --outfile "$ipd_prefix.out"
+
+    break
+done
---
+++ benchmark/compare/batch_modifi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=modifi_28        # Job name
+#SBATCH --partition=standard        # Partition name
+
+set -euo pipefail
+
+ref_dir=/home/shuaiw/borg/contigs/
+ref_set=(test_100.fa test_200.fa test_300.fa test_500.fa)
+modifi_out_root=/home/shuaiw/borg/paper/ipdsummary/soil_1/modifi.out/
+threads="${SLURM_CPUS_ON_NODE:-64}"
+
+mkdir -p "$modifi_out_root"
+
+for ref in "${ref_set[@]}"; do
+    base="$(basename "$ref" .fa)"
+    ref_path="$ref_dir/$ref"
+    work_dir="$modifi_out_root/$base"
+    align_bam="$work_dir/$base.align.subreads.bam"
+
+    mkdir -p "$work_dir"
+
+    /usr/bin/time -v -o "$work_dir/modifi.host.time" \
+        python /home/shuaiw/MODIFI/main.py \
+        --work_dir "$work_dir" \
+        --whole_bam "$align_bam" \
+        --whole_ref "$ref_path" \
+        --read_type subreads \
+        --run_steps split load control compare \
+        --threads $threads
+    break
+done
